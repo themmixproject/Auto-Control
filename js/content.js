00be81dddb550c8b83946b614337b18ba69fa170
themmixproject <steven.devalk@gmail.com>
2023-05-05 23:58:32 +0200

    Added overlay that wraps over element that the mouse is on, checks if mouse is not hovering over document or body, content.css file for overlay styling, imported overlay styling in manifest.json

--- js/content.js
+++ js/content.js
@@ -1,9 +1,34 @@
 console.log("hello world!");
 
-document.addEventListener("click", function(event){
-    var x = event.pageX;
-    var y = event.pageY;
-    var myElement = document.elementFromPoint(x - window.scrollX, y - window.scrollY);
+var overlay = document.createElement("div");
+overlay.className = "autocontrol-overlay";
+document.body.appendChild(overlay);
+overlay.style.display = "none";
 
-    console.log(myElement);
+
+var selectedElement = null;
+
+document.addEventListener("mousemove", function (event) {
+    overlay.style.display = "none";
+
+    var hoverElement = document.elementFromPoint(
+        event.pageX - window.scrollX,
+        event.pageY - window.scrollY
+    );
+    if(hoverElement == document.body || hoverElement == document.documentElement){
+        console.log("stop run")
+        overlay.style.display = "none";
+        return;
+    }
+
+    overlay.style.display = "";
+    selectedElement = hoverElement;
+
+    var boundingClientRect = hoverElement.getBoundingClientRect();
+
+    overlay.style.top = boundingClientRect.top + window.scrollY + "px";
+    overlay.style.left = boundingClientRect.left + "px";
+
+    overlay.style.height = boundingClientRect.height + "px";
+    overlay.style.width = boundingClientRect.width + "px";
 });
