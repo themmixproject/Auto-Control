a1039fdf2f900b4568da9a4dfc204988f2f1d441
themmixproject <steven.devalk@gmail.com>
2023-05-05 20:38:03 +0200

    Added semicolon at the end of "hello world!" log, added click event listener that retrieves clicked element at coordinates

--- js/content.js
+++ js/content.js
@@ -1,1 +1,9 @@
-console.log("hello world!")
+console.log("hello world!");
+
+document.addEventListener("click", function(event){
+    var x = event.pageX;
+    var y = event.pageY;
+    var myElement = document.elementFromPoint(x - window.scrollX, y - window.scrollY);
+
+    console.log(myElement);
+});
