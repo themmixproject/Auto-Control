2f06bcdf390be2626940ef5ab963a7766160688a
themmixproject <steven.devalk@gmail.com>
2023-05-06 22:24:40 +0200

    Added floating window on page for creating processes, added close button to floating Auto Control window, added generateApp() method for initialization to prevent unneeded memory, added prevention to select body, HTML elements, added prevention if hovered element is null, added direct HTML string appending instead of programmitcally generating elements

--- js/content.js
+++ js/content.js
@@ -1,39 +1,65 @@
 console.log("hello world!");
 
-var overlay = document.createElement("div");
-overlay.className = "autocontrol-overlay";
-document.body.appendChild(overlay);
-overlay.style.display = "none";
+var appContainer = document.createElement("div");
+document.body.prepend(appContainer);
+function generateApp() {
+    appContainer.innerHTML +=
+        "<div class='autocontrol-overlay'></div>" +
+        "<div id='autocontrol-window'>" +
+        "<header id='autocontrol-window-header'>" +
+        "Auto Control" +
+        "<button id='autocontrol-window-close-button'>X</button>" +
+        "</header>" +
+        "<div id='autocontrol-window-content'>content" +
+        "</div>";
 
+    var selectedElement = null;
 
-var selectedElement = null;
-
-document.addEventListener("mousemove", function (event) {
-    overlay.style.display = "none";
-
-    var hoverElement = document.elementFromPoint(
-        event.pageX - window.scrollX,
-        event.pageY - window.scrollY
-    );
-    if(hoverElement == document.body || hoverElement == document.documentElement){
+    var overlay = document.getElementsByClassName("autocontrol-overlay")[0];
+    document.addEventListener("mousemove", function (event) {
         overlay.style.display = "none";
-        return;
-    }
 
-    overlay.style.display = "";
-    selectedElement = hoverElement;
+        var hoverElement = document.elementFromPoint(
+            event.pageX - window.scrollX,
+            event.pageY - window.scrollY
+        );
 
-    var boundingClientRect = hoverElement.getBoundingClientRect();
+        var autoControlWindow = document.getElementById("autocontrol-window");
+        var isWindow =
+            autoControlWindow.contains(hoverElement) ||
+            hoverElement == autoControlWindow;
 
-    overlay.style.top = boundingClientRect.top + window.scrollY + "px";
-    overlay.style.left = boundingClientRect.left + "px";
+        if (
+            hoverElement == null ||
+            hoverElement == document.body ||
+            hoverElement == document.documentElement ||
+            isWindow
+        ) {
+            overlay.style.display = "none";
+            return;
+        }
 
-    overlay.style.height = boundingClientRect.height + "px";
-    overlay.style.width = boundingClientRect.width + "px";
-});
+        overlay.style.display = "";
+        selectedElement = hoverElement;
 
+        var boundingClientRect = hoverElement.getBoundingClientRect();
 
+        overlay.style.top = boundingClientRect.top + window.scrollY + "px";
+        overlay.style.left = boundingClientRect.left + "px";
 
-document.addEventListener("click", function(event){
-    console.log(selectedElement);
-})
+        overlay.style.height = boundingClientRect.height + "px";
+        overlay.style.width = boundingClientRect.width + "px";
+    });
+
+    document
+        .getElementById("autocontrol-window-close-button")
+        .addEventListener("click", function () {
+            console.log("close Auto Control window");
+        });
+
+    document.addEventListener("click", function (event) {
+        console.log(selectedElement);
+    });
+}
+
+generateApp();
