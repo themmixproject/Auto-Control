9ce53c9d61f1118915a540990449c3e93cecb74f
themmixproject <steven.devalk@gmail.com>
2023-05-12 22:57:50 +0200

    AutoControl window opens when user clicks on extension icon, AutoControl window closes when user clicks on the "X" button, app will not get generated if app is already appeneded to body, when close button is clicked eventListeners will be removed from body, eventListener functions "moveOverlayToElement" and "setSelectedElement" have been added

--- js/content.js
+++ js/content.js
@@ -1,7 +1,11 @@
 console.log("hello world!");
 
-var appContainer = document.createElement("div");
-document.body.prepend(appContainer);
+var appContainer = null;
+var selectedElement = null;
+var overlay = null;
+
+appContainer = document.createElement("div");
+
 function generateApp() {
     appContainer.innerHTML +=
         "<div class='autocontrol-overlay'></div>" +
@@ -13,57 +17,67 @@ function generateApp() {
         "<div id='autocontrol-window-content'>content" +
         "</div>";
 
-    var selectedElement = null;
-
-    var overlay = document.getElementsByClassName("autocontrol-overlay")[0];
-    document.addEventListener("mousemove", function (event) {
-        overlay.style.display = "none";
-
-        var hoverElement = document.elementFromPoint(
-            event.pageX - window.scrollX,
-            event.pageY - window.scrollY
-        );
-
-        var autoControlWindow = document.getElementById("autocontrol-window");
-        var isWindow =
-            autoControlWindow.contains(hoverElement) ||
-            hoverElement == autoControlWindow;
-
-        if (
-            hoverElement == null ||
-            hoverElement == document.body ||
-            hoverElement == document.documentElement ||
-            isWindow
-        ) {
-            overlay.style.display = "none";
-            return;
-        }
-
-        overlay.style.display = "";
-        selectedElement = hoverElement;
-
-        var boundingClientRect = hoverElement.getBoundingClientRect();
-
-        overlay.style.top = boundingClientRect.top + window.scrollY + "px";
-        overlay.style.left = boundingClientRect.left + "px";
-
-        overlay.style.height = boundingClientRect.height + "px";
-        overlay.style.width = boundingClientRect.width + "px";
-    });
+    overlay = document.getElementsByClassName("autocontrol-overlay")[0];
+    document.addEventListener("mousemove", moveOverlayToElement);
 
     document
         .getElementById("autocontrol-window-close-button")
         .addEventListener("click", function () {
-            console.log("close Auto Control window");
+            document.removeEventListener("click", setSelectedElement);
+            document.removeEventListener("mousemove", moveOverlayToElement);
+            document.body.removeChild(appContainer);
+
+            selectedElement = null;
+            appContainer = null;
+            overlay = null;
         });
 
-    document.addEventListener("click", function (event) {
-        console.log(selectedElement);
-    });
-
-    chrome.runtime.onMessage.addListener(function(request, sender){
-        console.log(request.message);
-    })
+    document.addEventListener("click", setSelectedElement);
 }
 
-generateApp();
+function moveOverlayToElement(event) {
+    overlay.style.display = "none";
+
+    var hoverElement = document.elementFromPoint(
+        event.pageX - window.scrollX,
+        event.pageY - window.scrollY
+    );
+
+    var autoControlWindow = document.getElementById("autocontrol-window");
+    var isWindow =
+        autoControlWindow.contains(hoverElement) ||
+        hoverElement == autoControlWindow;
+
+    if (
+        hoverElement == null ||
+        hoverElement == document.body ||
+        hoverElement == document.documentElement ||
+        isWindow
+    ) {
+        overlay.style.display = "none";
+        return;
+    }
+
+    overlay.style.display = "";
+    selectedElement = hoverElement;
+
+    var boundingClientRect = hoverElement.getBoundingClientRect();
+
+    overlay.style.top = boundingClientRect.top + window.scrollY + "px";
+    overlay.style.left = boundingClientRect.left + "px";
+
+    overlay.style.height = boundingClientRect.height + "px";
+    overlay.style.width = boundingClientRect.width + "px";
+}
+
+function setSelectedElement() {
+    console.log(selectedElement);
+}
+
+chrome.runtime.onMessage.addListener(function (request, sender) {
+    var firstChild = document.body.children[0]
+    var lastChildIndex = firstChild.children.length - 1;
+    if(firstChild.children[lastChildIndex].id != "autocontrol-window"){
+        generateApp();
+    }
+});
