f1d4b0edd4ca519afc1e15b2723616488f3704d1
themmixproject <steven.devalk@gmail.com>
2023-06-06 22:55:22 +0200

    Formatted background.js

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
-chrome.action.onClicked.addListener(function(tab){
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+chrome.action.onClicked.addListener(function (tab) {
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         var activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
-    })
-})
+        chrome.tabs.sendMessage(activeTab.id, {
+            message: "clicked_browser_action",
+        });
+    });
+});
