cb59e7c98f5cb8151d562367e53a575e7a0aa2c3
themmixproject <steven.devalk@gmail.com>
2023-08-20 23:09:14 +0200

    Rewrote rendering for element cards [ONLY CURRENTLY WORKS ON GROUP ELEMENTS]

--- js/elementCard.js
+++ js/elementCard.js
@@ -1,19 +1,22 @@
-function generateGroupCard(elements){
-    var listItem = document.createElement("div");
-    listItem.className = "autocontrol-element-list-item item-group";
+function generateGroupCard(elements) {
+    console.log("elements: ", elements);
 
-    var listItemHeader = generateGroupCardHeader(elements, listItem);
-    listItem.append(listItemHeader);
 
-    var listItemContent = generateCardContent(elements[0]);
-    listItem.appendChild(listItemContent);
+    var elementCard = document.createElement("div");
+    elementCard.className = "autocontrol-element-list-item item-group";
+
+    var header = generateCardHeader(elements, elementCard);
+    elementCard.append(header);
+
+    var content = generateCardContent(elements);
+    elementCard.appendChild(content);
 
     var elementList = document.getElementById("autocontrol-element-list");
-    elementList.appendChild(listItem);
+    elementList.appendChild(elementCard);
 }
 
-function generateGroupCardHeader(elements, listItem) {
-    var listItemHeader = document.createElement("header");
+function generateCardHeader(elements, elementCard) {
+    var header = document.createElement("header");
     var baseElement = elements[0];
 
     var infoContainer = document.createElement("span");
@@ -21,120 +24,65 @@ function generateGroupCardHeader(elements, listItem) {
     infoContainer.style.overflow = "hidden";
     infoContainer.style.textOverflow = "ellipsis";
     infoContainer.style.whiteSpace = "nowrap";
-    
+
     infoContainer.innerHTML =
-    "<span style='color: blue'>" +
-    baseElement.tagName.toLowerCase() +
-    "</span>";
+        "<span style='color: blue'>" +
+        baseElement.tagName.toLowerCase() +
+        "</span>";
     if (baseElement.id) {
         infoContainer.innerHTML +=
-        "<span style='color: green;'>#" + baseElement.id + "</span>";
+            "<span style='color: green;'>#" + baseElement.id + "</span>";
     }
     if (baseElement.className) {
         infoContainer.innerHTML +=
-        "<span style='color: red'>." + baseElement.className + "</span>";
+            "<span style='color: red'>." + baseElement.className + "</span>";
     }
-    infoContainer.innerHTML += " [<span style='color: orange'>" + elements.length +"</span>]";
-    listItemHeader.appendChild(infoContainer);
+    if (elements.length > 1) {
+        infoContainer.innerHTML += " [<span style='color: orange'>" + elements.length +"</span>]";
+        header.appendChild(infoContainer);
+    }
 
     var deleteButton = document.createElement("button");
     deleteButton.textContent = "Delete";
     deleteButton.className = "delete-button";
     deleteButton.addEventListener("click", function () {
-        listItem.remove();
+        elementCard.remove();
     });
-    listItemHeader.appendChild(deleteButton);
+    
+    header.appendChild(deleteButton);
 
-
-
-
-    return listItemHeader;
-
+    return header;
 }
 
-function generateElementCard(element) {
-    var elementListItem = document.createElement("div");
-    elementListItem.className = "autocontrol-element-list-item";
+function generateCardContent(elements) {
+    var baseElement = elements[0];
 
-    var listItemHeader = generateCardHeader(element, elementListItem);
-    elementListItem.append(listItemHeader);
+    var content = document.createElement("div");
+    content.className = "content";
 
-    var listItemContent = generateCardContent(element);
-    elementListItem.appendChild(listItemContent);
-
-    var elementList = document.getElementById("autocontrol-element-list");
-    elementList.appendChild(elementListItem);
-
-    return listItemContent;
-}
-
-function generateCardHeader(element, listItem) {
-    var listItemHeader = document.createElement("header");
-
-    var elementInfo = convertElementToStyledSelectorElement(element);
-    listItemHeader.appendChild(elementInfo);
-
-    var deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete";
-    deleteButton.className = "delete-button";
-    deleteButton.addEventListener("click", function () {
-        listItem.remove();
-    });
-
-    listItemHeader.appendChild(deleteButton);
-    return listItemHeader;
-}
-
-function convertElementToStyledSelectorElement(element) {
-    var elementInfo = document.createElement("span");
-    elementInfo.style.fontFamily = "monospace";
-    elementInfo.style.overflow = "hidden";
-    elementInfo.style.textOverflow = "ellipsis";
-    elementInfo.style.whiteSpace = "nowrap";
-
-    elementInfo.innerHTML =
-        "<span style='color: blue'>" +
-        element.tagName.toLowerCase() +
-        "</span>";
-    if (element.id) {
-        elementInfo.innerHTML +=
-            "<span style='color: green;'>#" + element.id + "</span>";
+    var actionDescriber = document.createElement("span");
+    actionDescriber.className = "action-descriptor";
+    
+    var conditionalContent = null;
+    var tag = baseElement.tagName.toLowerCase();
+    if (tag == "a") {
+        actionDescriber.inneHTML += "got to: ";
+        conditionalContent = generateLinkContent(baseElement)
     }
-    if (element.className) {
-        elementInfo.innerHTML +=
-            "<span style='color: red'>." + element.className + "</span>";
+    else if (tag == "div" || tag == "button") {
+        actionDescriber.innerHTML += "action: ";
+
+        conditionalContent = generateClickActionContent(baseElement);
     }
+    else if(tag == "input") {
+        actionDescriber += "inster value: ";
 
-    return elementInfo;
-}
-
-function generateCardContent(element) {
-    var contentContainer = document.createElement("div");
-    contentContainer.className = "content";
-
-    var actionDescriptor = document.createElement("span");
-    actionDescriptor.className = "action-descriptor";
-
-    var generatedElement = null;
-    var elementTag = element.tagName.toLowerCase();
-    if (elementTag === "a") {
-        actionDescriptor.innerHTML += "go to: ";
-
-        generatedElement = generateLinkContent(element);
-    } else if (elementTag === "div" || elementTag === "button") {
-        actionDescriptor.innerHTML += "action: ";
-
-        generatedElement = generateClickActionContent();
-    } else if (elementTag === "input") {
-        actionDescriptor.innerHTML += "insert value: ";
-
-        generatedElement = generateInputContent(element);
+        conditionalContent = generateInputContent(baseElement);
     }
+    content.appendChild(actionDescriber);
+    content.appendChild(conditionalContent)
 
-    contentContainer.appendChild(actionDescriptor);
-    contentContainer.appendChild(generatedElement);
-
-    return contentContainer;
+    return content;
 }
 
 function generateLinkContent(element) {
@@ -155,10 +103,10 @@ function generateLinkContent(element) {
 }
 
 function generateClickActionContent() {
-    var actionElement = document.createElement("span");
-    actionElement.innerHTML += "click";
-
-    return actionElement;
+    var description = document.createElement("span");
+    description.innerHTML += "click";
+    
+    return description;
 }
 
 function generateInputContent(element) {
@@ -178,3 +126,177 @@ function generateInputContent(element) {
 
     return inputContainer;
 }
+
+// function generateGroupCardHeader(elements, listItem) {
+//     var listItemHeader = document.createElement("header");
+//     var baseElement = elements[0];
+
+//     var infoContainer = document.createElement("span");
+//     infoContainer.style.fontFamily = "monospace";
+//     infoContainer.style.overflow = "hidden";
+//     infoContainer.style.textOverflow = "ellipsis";
+//     infoContainer.style.whiteSpace = "nowrap";
+
+//     infoContainer.innerHTML =
+//     "<span style='color: blue'>" +
+//     baseElement.tagName.toLowerCase() +
+//     "</span>";
+//     if (baseElement.id) {
+//         infoContainer.innerHTML +=
+//         "<span style='color: green;'>#" + baseElement.id + "</span>";
+//     }
+//     if (baseElement.className) {
+//         infoContainer.innerHTML +=
+//         "<span style='color: red'>." + baseElement.className + "</span>";
+//     }
+//     infoContainer.innerHTML += " [<span style='color: orange'>" + elements.length +"</span>]";
+//     listItemHeader.appendChild(infoContainer);
+
+//     var deleteButton = document.createElement("button");
+//     deleteButton.textContent = "Delete";
+//     deleteButton.className = "delete-button";
+//     deleteButton.addEventListener("click", function () {
+//         listItem.remove();
+//     });
+//     listItemHeader.appendChild(deleteButton);
+
+//     return listItemHeader;
+
+// }
+
+// function generateElementCard(element) {
+//     var elementListItem = document.createElement("div");
+//     elementListItem.className = "autocontrol-element-list-item";
+
+//     var listItemHeader = generateCardHeader(element, elementListItem);
+//     elementListItem.append(listItemHeader);
+
+//     var listItemContent = generateCardContent(element);
+//     elementListItem.appendChild(listItemContent);
+
+//     var elementList = document.getElementById("autocontrol-element-list");
+//     elementList.appendChild(elementListItem);
+
+//     return listItemContent;
+// }
+
+// function generateCardHeader(element, listItem) {
+//     var listItemHeader = document.createElement("header");
+
+//     var elementInfo = convertElementToStyledSelectorElement(element);
+//     listItemHeader.appendChild(elementInfo);
+
+//     var deleteButton = document.createElement("button");
+//     deleteButton.textContent = "Delete";
+//     deleteButton.className = "delete-button";
+//     deleteButton.addEventListener("click", function () {
+//         listItem.remove();
+//     });
+
+//     listItemHeader.appendChild(deleteButton);
+//     return listItemHeader;
+// }
+
+// function convertElementToStyledSelectorElement(element) {
+//     var elementInfo = document.createElement("span");
+//     elementInfo.style.fontFamily = "monospace";
+//     elementInfo.style.overflow = "hidden";
+//     elementInfo.style.textOverflow = "ellipsis";
+//     elementInfo.style.whiteSpace = "nowrap";
+
+//     elementInfo.innerHTML =
+//         "<span style='color: blue'>" +
+//         element.tagName.toLowerCase() +
+//         "</span>";]
+//     if (element.id) {
+//         elementInfo.innerHTML +=
+//             "<span style='color: green;'>#" + element.id + "</span>";
+//     }
+//     if (element.className) {
+//         elementInfo.innerHTML +=
+//             "<span style='color: red'>." + element.className + "</span>";
+//     }
+
+//     return elementInfo;
+// }
+
+// function generateCardContent(element) {
+//     var contentContainer = document.createElement("div");
+//     contentContainer.className = "content";
+
+//     var actionDescriptor = document.createElement("span");
+//     actionDescriptor.className = "action-descriptor";
+
+//     var generatedElement = null;
+//     var elementTag = element.tagName.toLowerCase();
+//     if (elementTag === "a") {
+//         actionDescriptor.innerHTML += "go to: ";
+
+//         generatedElement = generateLinkContent(element);
+//     } else if (elementTag === "div" || elementTag === "button") {
+//         actionDescriptor.innerHTML += "action: ";
+
+//         generatedElement = generateClickActionContent();
+//     } else if (elementTag === "input") {
+//         actionDescriptor.innerHTML += "insert value: ";
+
+//         generatedElement = generateInputContent(element);
+//     }
+
+//     contentContainer.appendChild(actionDescriptor);
+//     contentContainer.appendChild(generatedElement);
+
+//     return contentContainer;
+// }
+
+// function generateLinkContent(element) {
+//     var linkElement = document.createElement("a");
+
+//     var url = new URL(element.href, window.location);
+//     if (url.protocol === "http:" || url.protocol === "https:") {
+//         linkElement.innerHTML += element.href;
+//         linkElement.href += element.href;
+//     } else {
+//         var root = window.location.origin;
+//         linkElement.innerHTML += root + element.href;
+//         linkElement.href += root + element.href;
+//     }
+//     linkElement.target = "_blank";
+
+//     return linkElement;
+// }
+
+// function generateClickActionContent() {
+//     var actionElement = document.createElement("span");
+//     actionElement.innerHTML += "click";
+
+//     return actionElement;
+// }
+
+// function generateInputContent(element) {
+//     var inputContainer = document.createElement("div");
+//     inputContainer.className = "input-container";
+
+//     var inputLabelWrapper = document.createElement("div");
+//     inputLabelWrapper.className = "label-wrapper";
+//     var inputLabel = document.createElement("label");
+//     inputLabel.innerHTML = element.placeholder;
+//     inputLabelWrapper.appendChild(inputLabel);
+
+//     inputContainer.appendChild(inputLabelWrapper);
+
+//     var input = document.createElement("input");
+//     inputContainer.appendChild(input);
+
+//     return inputContainer;
+// }
+
+loadApp();
+
+var demoButton = document.querySelector(
+    "html body:nth-child(2) div#app:nth-child(2) div#card-container:nth-child(9) div.card:nth-child(3) button:nth-child(5)"
+);
+console.log(findElementGroup(demoButton, 5));
+
+var buttonGroup = findElementGroup(demoButton, 5);
+generateGroupCard(buttonGroup);
